205402a058fe7ddfe413691467470792d51e9acc
Ben West <btwest@amazon.com>
2024-08-12 17:37:52 -0400

    initial commit

---
+++ src/aws-exports.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+
+export default {
+    "aws_project_region": "PLACEHOLDER_REGION",
+    "aws_cognito_identity_pool_id": "PLACEHOLDER_IDENTITY_POOL_ID",
+    "aws_cognito_region": "PLACEHOLDER_COGNITO_REGION",
+    "aws_user_pools_id": "PLACEHOLDER_USER_POOL_ID",
+    "aws_user_pools_web_client_id": "PLACEHOLDER_USER_POOL_WEB_CLIENT_ID",
+    "bucket_name": "PLACEHOLDER_BUCKET_NAME",
+    "balanced_model_id": "PLACEHOLDER_BALANCED_MODEL_ID",
+    "fast_model_id": "PLACEHOLDER_FAST_MODEL_ID",
+    "raw_folder": "PLACEHOLDER_RAW_FOLDER",
+    "video_script_folder": "PLACEHOLDER_VIDEO_SCRIPT_FOLDER",
+    "video_caption_folder": "PLACEHOLDER_VIDEO_CAPTION_FOLDER",
+    "transcription_folder": "PLACEHOLDER_TRANSCRIPTION_FOLDER",
+    "entity_sentiment_folder": "PLACEHOLDER_ENTITY_SENTIMENT_FOLDER",
+    "summary_folder": "PLACEHOLDER_SUMMARY_FOLDER",
+    "rest_api_url": "PLACEHOLDER_REST_API_URL",
+    "videos_api_resource": "PLACEHOLDER_VIDEOS_API_RESOURCE"
+}
